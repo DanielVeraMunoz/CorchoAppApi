c68f7d3555c27ed19eec8a59ee9d8bef3866d43c
Daniel Vera <dani.vmg91@gmail.com>
2026-03-24 12:48:23 +0100

    fix: Add use HasFactory in all models for testing

--- app/Models/Thank.php
+++ app/Models/Thank.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Thank extends Model
 {
+
+    use HasFactory;
+
     protected $fillable = [
         'note_id',
         'giver_id',
