82057057c92f57fe0a9a4e008400e01f72c9da32
Daniel Vera <dani.vmg91@gmail.com>
2026-03-24 13:25:09 +0100

    fix: Add datas in AuthController for do the test in green

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    
+    public function register(Request $request)
+    {
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'community_id' => $request->community_id,
+            'floor' => $request->floor,
+            'door' => $request->door,
+            'role' => 'user',
+    ]);
+
+        return response()->json([
+            'message' => 'Usuario registrado correctamente',
+            'data' => $user
+         ], 201);
+
+
+
+    }
+
+
+
 }
