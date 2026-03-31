16cc8b06b77fee87d16820908df4e9b3a2d03ecf
Daniel Vera <dani.vmg91@gmail.com>
2026-03-31 23:16:45 +0200

    Add data to NoteFactory

--- database/factories/NoteFactory.php
+++ database/factories/NoteFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Category;
 use App\Models\Note;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -18,7 +20,12 @@ class NoteFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'event_date' => $this->faker->dateTimeBetween('now', '+1 month'),
+            'is_completed' => $this->faker->boolean(),
         ];
     }
 }
