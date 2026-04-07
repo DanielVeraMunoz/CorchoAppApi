a34b2b1862b75ec534896abd4b3421d13274b57d
Daniel Vera <dani.vmg91@gmail.com>
2026-04-07 12:39:37 +0200

    Add data in store for create comment in Controller

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, $noteId){
+
+        $comment = Comment::create([
+            'note_id' => $noteId,
+            'user_id' => $request->user()->id,
+            'content' => $request->content,
+        ]);
+
+        response()->json([
+            'message' => 'Comentario creado correctamente',
+            'data' => $comment,
+        ], 201);
+    }
 }
