673d1bb867dabf279d4266ace82793ae2072e076
Daniel Vera <dani.vmg91@gmail.com>
2026-04-07 12:53:06 +0200

    Refactor Create Comment with request

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Comment;
+use App\Http\Requests\StoreCommentRequest;
 
 class CommentController extends Controller
 {
-    public function store(Request $request, $noteId){
+    public function store(StoreCommentRequest $request, $noteId){
 
         $comment = Comment::create([
             'note_id' => $noteId,
