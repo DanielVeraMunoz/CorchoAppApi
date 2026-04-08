fbc037dd0842a962d271a24a23589f00d9a823c9
Daniel Vera <dani.vmg91@gmail.com>
2026-04-08 12:47:03 +0200

    Fix: no user, is recipient

--- app/Http/Controllers/Api/StatsController.php
+++ app/Http/Controllers/Api/StatsController.php
@@ -10,14 +10,18 @@ class StatsController extends Controller
     public function communityStats(Request $request)
     {
         $communityId = $request->user()->community_id;
+
         $totalUsers = \App\Models\User::where('community_id', $communityId)->count();
+
         $totalNotes = \App\Models\Note::whereHas('user', function ($query) use ($communityId) {
             $query->where('community_id', $communityId);
         })->count();
+
         $totalComments = \App\Models\Comment::whereHas('note.user', function ($query) use ($communityId) {
             $query->where('community_id', $communityId);
         })->count();
-        $totalThanks = \App\Models\Thank::whereHas('note.user', function ($query) use ($communityId) {
+
+        $totalThanks = \App\Models\Thank::whereHas('recipient', function ($query) use ($communityId) {
             $query->where('community_id', $communityId);
         })->count();
 
