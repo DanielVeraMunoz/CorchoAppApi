ea52821af608d139c299168ed6cb68b1498fd218
Daniel Vera <dani.vmg91@gmail.com>
2026-04-08 10:41:50 +0200

    Create user can list initial code for TDD

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    // public function test_example(): void
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+
+    public function test_authenticated_user_can_list_users()
+    {
+        $user = \App\Models\User::factory()->create();
+        $token = $user->createToken('auth_token')->accessToken;
+
+        \App\Models\User::factory()->count(5)->create();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+        ])->getJson('/api/users');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(6, 'data'); // 5 usuarios creados + el usuario autenticado
+    }
+
+}
